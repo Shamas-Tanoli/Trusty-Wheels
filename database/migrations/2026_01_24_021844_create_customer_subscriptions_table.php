8b90d225066befad734b4c2c57437921b9901f55
Shamas-Tanoli <developershamas@gmail.com>
2026-01-26 23:40:59 +0500

    fix: customer subscription migration

--- database/migrations/2026_01_24_021844_create_customer_subscriptions_table.php
+++ database/migrations/2026_01_24_021844_create_customer_subscriptions_table.php
@@ -20,8 +20,8 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date')->nullable();
             $table->decimal('price', 10, 2); 
-            $table->enum('payment_type', ['one_time','weekly','monthly']);
-            $table->enum('status', ['active','paused','cancelled'])->default('active');
+            $table->enum('payment_type', ['one_time','monthly']);
+            $table->enum('status', ['pending','active','paused','cancelled'])->default('active');
             $table->timestamps(); 
         });
     }

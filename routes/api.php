f9a6e5b5aba16594379c6b062779f3fdcdeb6eb8
Shamas-Tanoli <developershamas@gmail.com>
2026-02-02 23:57:28 +0500

    fix: refactor

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,6 @@ Route::middleware(['auth:sanctum', 'role:customer'])->group(function () {
     Route::get('/customer/bookings/all', [BookingController::class, 'customerbooking']);
     Route::post('/passenger/status', [BookingController::class, 'passengerStatus']);
     Route::get('/customer/children', [ServiceJobPassangerController::class, 'getChildrenWithJobs']);
-
     Route::post('/customer/booking/{id}/passenger/add', [BookingController::class, 'addChildren']);
 });
 

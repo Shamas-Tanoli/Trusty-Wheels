e5782e581ee7369ee6a38995728d473e8e7819c9
Shamas-Tanoli <developershamas@gmail.com>
2026-02-09 19:24:25 +0500

    feat: migration for inquiry

--- database/migrations/2026_02_09_191953_create_inquiries_table.php
+++ database/migrations/2026_02_09_191953_create_inquiries_table.php
@@ -1,25 +1,28 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('inquiries', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('vehicle_id');
+
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone');
+            $table->text('message')->nullable();
+
             $table->timestamps();
+
+            // Foreign key
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('inquiries');

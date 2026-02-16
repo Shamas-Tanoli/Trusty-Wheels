35ee3233c35173189055c970d838d7785773c603
Shamas-Tanoli <developershamas@gmail.com>
2026-02-16 22:09:34 +0500

    notification

---
+++ database/migrations/2026_02_16_220507_create_notifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->index();
+            $table->string('title', 255);
+            $table->text('body');
+            $table->json('data')->nullable();
+            $table->string('type', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};

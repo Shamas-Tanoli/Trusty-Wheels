dd44a6807debb815694c015e841c6c6b7d27a983
Shamas-Tanoli <developershamas@gmail.com>
2026-02-16 22:43:51 +0500

    notification

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -10,7 +10,7 @@ class NotificationController extends Controller
 {
     public function getUserNotifications(Request $request)
     {
-        // Validate input
+      
         $request->validate([
             'user_id'   => 'required|integer',
             'user_type' => 'required|string',
@@ -19,7 +19,7 @@ class NotificationController extends Controller
         $userId   = $request->input('user_id');
         $userType = $request->input('user_type');
 
-        // Fetch notifications from DB
+        
         $notifications = DB::table('notifications')
             ->where('user_id', $userId)
             ->where('user_type', $userType)
